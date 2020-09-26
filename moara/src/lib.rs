3d216d8659ade7302fee90f2e1bfdc713c38c70d
Sorin Bolos <sorin.bolos@uipath.com>
2020-09-27 01:08:51 +0300

    Return dict from rust

--- moara/src/lib.rs
+++ moara/src/lib.rs
@@ -10,6 +10,7 @@ pub mod gates;
 
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use std::collections::HashMap;
 
 pub fn simulate_with_uranium(serialized_circuit:String, shots:u32, qubit_count:Option<u8>) -> Vec<u32>
 {
@@ -17,9 +18,16 @@ pub fn simulate_with_uranium(serialized_circuit:String, shots:u32, qubit_count:O
 }
 
 #[pyfunction]
-pub fn simulate_qiskit(serialized_circuit:String, shots:u32, qubit_count:Option<u8>) -> PyResult<String>
+#[text_signature = "(serialized_circuit, shots, qubit_count)"]
+pub fn simulate_qiskit(serialized_circuit:String, shots:u32, qubit_count:Option<u8>) -> PyResult<HashMap<String, u32>>
 {
-    Ok("Hello from rust".to_string())
+    let mut results = HashMap::new();
+    results.insert("00".to_string(), 1);
+    results.insert("01".to_string(), 2);
+    results.insert("10".to_string(), 3);
+    results.insert("11".to_string(), shots-6);
+
+    Ok(results)
 }
 
 #[pymodule]
