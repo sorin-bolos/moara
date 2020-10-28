b6b60de6f27cd8029ee2cf63d487f074cd8ff372
Sorin Bolos <sorin.bolos@uipath.com>
2020-10-28 21:06:43 +0200

    Pad results with 0s

--- moara_for_qiskit/src/lib.rs
+++ moara_for_qiskit/src/lib.rs
@@ -23,11 +23,28 @@ fn moara_for_qiskit(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 fn convert_to_hashmap(result_vector:Vec<u32>) -> HashMap<String, u32> {
+    let qubit_count = (result_vector.len() as f64).log2() as usize;
     let mut results = HashMap::new();
     for (i, count) in result_vector.iter().enumerate() {
         if *count > 0u32 {
-            results.insert(format!("{:b}", i), *count);
+            let binary = format!("{:b}", i);
+            let padded = pad_to_width(binary, qubit_count);
+            results.insert(padded, *count);
         }
     }
     results
+}
+
+fn pad_to_width(original:String, width:usize) -> String {
+    if width <= original.len() {
+        return original;
+    }
+    
+    let pad_count = width - original.len();
+    let mut padded = String::with_capacity(width);
+    for _ in 0..pad_count {
+        padded.push_str("0");
+    }
+    padded.push_str(&original);
+    padded
 }
