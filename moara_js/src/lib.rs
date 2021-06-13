c92a980e091122b30d98599bb559cd1ce2520e9b
Sorin Bolos <sorin.bolos@uipath.com>
2021-06-13 20:27:05 +0300

    Add get_probabilities ro moara_js

--- moara_js/src/lib.rs
+++ moara_js/src/lib.rs
@@ -5,3 +5,8 @@ use moara;
 pub fn simulate(serialized_circuit:String, shots:u32, qubit_count:Option<u8>) -> Vec<u32> {
     moara::simulate(serialized_circuit, shots, qubit_count)
 }
+
+#[wasm_bindgen]
+pub fn get_probabilities(serialized_circuit:String, qubit_count:Option<u8>) -> Vec<f32> {
+    moara::get_probabilities(serialized_circuit, qubit_count)
+}
