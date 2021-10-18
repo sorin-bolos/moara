9bba5ae5946839a0bdc4f3c2c76399a0d94be356
Sorin Bolos <sorin.bolos@uipath.com>
2021-10-19 00:34:11 +0300

    Add cli help

--- moara/src/main.rs
+++ moara/src/main.rs
@@ -6,20 +6,40 @@ use structopt::StructOpt;
 
 use moara;
 
-// const USAGE:&str = "Usage: 'moara.exe circuit_filename.json 1024 4'";
-// const INVALID_ARGUMENT_COUNT:&str = "Invalid number of arguments. Need to suply at least one argument.";
-// const COULD_NOT_PARSE_QUBIT_COUNT:&str = "Could not parse argument for 'qubit_count'.";
-
 fn main() {
     let config = Config::from_args();
 
-    let serialized_circuit = read_file(config.circuit_filename).unwrap_or_else(|err| {
-        println!("{}", err);
-        process::exit(1);
-    });
+    match config.command {
+        Command::Sample { circuit_filename, qubit_count, shots } => {
+            let serialized_circuit = read_file(circuit_filename).unwrap_or_else(|err| {
+                println!("{}", err);
+                process::exit(1);
+            });
 
-    let results = moara::get_statevector(serialized_circuit, config.qubit_count);
-    print!("{:?}", results);
+            let results = moara::simulate(serialized_circuit, shots, qubit_count);
+            print!("{:?}", results);
+        },
+        Command::Probabilities { circuit_filename, qubit_count } => {
+            let serialized_circuit = read_file(circuit_filename).unwrap_or_else(|err| {
+                println!("{}", err);
+                process::exit(1);
+            });
+
+            let results = moara::get_probabilities(serialized_circuit, qubit_count);
+            print!("{:?}", results);
+        },
+        Command::Statevector { circuit_filename, qubit_count } => {
+            let serialized_circuit = read_file(circuit_filename).unwrap_or_else(|err| {
+                println!("{}", err);
+                process::exit(1);
+            });
+
+            let results = moara::get_statevector(serialized_circuit, qubit_count);
+            print!("{:?}", results);
+        },
+    }
+
+    
 }
 
 fn read_file(circuit_filename:PathBuf) -> Result<String, Box<dyn Error>>
@@ -30,9 +50,42 @@ fn read_file(circuit_filename:PathBuf) -> Result<String, Box<dyn Error>>
 }
 
 #[derive(StructOpt)]
+#[structopt(about = "Moara quatnum simultor")]
 struct Config {
-    #[structopt(parse(from_os_str))]
-    circuit_filename:PathBuf,
-    //shots:u32,
-    qubit_count:Option<u8>
+    #[structopt(flatten)]
+    command:Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    
+    #[structopt(about = "Sample the circuit over a number of shots")]
+    Sample {
+        #[structopt(parse(from_os_str))]
+        circuit_filename:PathBuf,
+    
+        #[structopt(short = "q", long = "qubits", help = "The number of qubits. Must be at least the width of the circuit.")]
+        qubit_count:Option<u8>,
+
+        #[structopt(short = "s", long = "shots", default_value = "1024", help = "The number of shots")]
+        shots:u32,
+    },
+    
+    #[structopt(about = "Get the final real probabilities")]
+    Probabilities{
+        #[structopt(parse(from_os_str))]
+        circuit_filename:PathBuf,
+    
+        #[structopt(short = "q", long = "qubits", help = "The number of qubits. Must be at least the width of the circuit.")]
+        qubit_count:Option<u8>
+    },
+    
+    #[structopt(about = "Get the final statevector")]
+    Statevector {
+        #[structopt(parse(from_os_str))]
+        circuit_filename:PathBuf,
+    
+        #[structopt(short = "q", long = "qubits", help = "The number of qubits. Must be at least the width of the circuit.")]
+        qubit_count:Option<u8>
+    }
 }
