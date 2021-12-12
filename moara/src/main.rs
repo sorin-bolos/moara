133a817d14ea299c68195c89f13f2ffce6da9786
Sorin Bolos <sorin.bolos@uipath.com>
2021-12-12 04:23:31 +0200

    Write to file

--- moara/src/main.rs
+++ moara/src/main.rs
@@ -1,3 +1,6 @@
+use num_complex::Complex32;
+use std::io::Write;
+use std::fs::File;
 use std::process;
 use std::fs;
 use std::error::Error;
@@ -10,43 +13,111 @@ fn main() {
     let config = Config::from_args();
 
     match config.command {
-        Command::Sample { circuit_filename, qubit_count, shots } => {
+        Command::Sample { circuit_filename, qubit_count, shots, output } => {
             let serialized_circuit = read_file(circuit_filename).unwrap_or_else(|err| {
                 println!("{}", err);
                 process::exit(1);
             });
 
             let results = moara::simulate(serialized_circuit, shots, qubit_count);
-            print!("{:?}", results);
+            
+            let results_str = format!("{:?}",results);
+            output_results(results_str, output);
         },
-        Command::Probabilities { circuit_filename, qubit_count } => {
+        Command::Probabilities { circuit_filename, qubit_count, output } => {
             let serialized_circuit = read_file(circuit_filename).unwrap_or_else(|err| {
                 println!("{}", err);
                 process::exit(1);
             });
 
             let results = moara::get_probabilities(serialized_circuit, qubit_count);
-            print!("{:?}", results);
+            let results_str = format_f32(results);
+            output_results(results_str, output);
         },
-        Command::Statevector { circuit_filename, qubit_count } => {
+        Command::Statevector { circuit_filename, qubit_count, output } => {
             let serialized_circuit = read_file(circuit_filename).unwrap_or_else(|err| {
                 println!("{}", err);
                 process::exit(1);
             });
 
             let results = moara::get_statevector(serialized_circuit, qubit_count);
-            print!("{:?}", results);
+            let results_str = format_complex32(results);
+            output_results(results_str, output);
         },
     }
 
     
 }
 
-fn read_file(circuit_filename:PathBuf) -> Result<String, Box<dyn Error>>
-{
+fn read_file(circuit_filename:PathBuf) -> Result<String, Box<dyn Error>> {
     let contents = fs::read_to_string(circuit_filename).unwrap();
     
     Ok(contents)
+}
+
+fn output_results(results_str:String, output:Option<PathBuf>) {
+    match output {
+        Some(filename) => {
+            let mut f = File::create(filename).unwrap_or_else(|err| {
+                println!("{}", err);
+                process::exit(1);
+            });
+            f.write_all(results_str.as_bytes()).unwrap_or_else(|err| {
+                println!("{}", err);
+                process::exit(1);
+            });
+        },
+        None => {
+            print!("{}", results_str);
+        }
+    }
+}
+
+fn format_f32(results:Vec<f32>) -> String {
+    let mut result = String::new();
+    result.push_str("[");
+    for r in &results[0..results.len() - 1] {
+        if *r == 0.0 || *r == 1.0 {
+            result.push_str(&r.to_string());
+        } else {
+            result.push_str(&format!("{:e}", r));
+        }
+        result.push_str(", ");
+    }
+    let r = &results[results.len()-1];
+    if *r == 0.0 || *r == 1.0 {
+        result.push_str(&r.to_string());
+    } else {
+        result.push_str(&format!("{:e}", r));
+    }
+    result.push_str("]");
+    
+    return result;
+}
+
+fn format_complex32(results:Vec<Complex32>) -> String {
+    let mut result = String::new();
+    let zero = Complex32::new(0.0,0.0);
+    let one = Complex32::new(1.0,0.0);
+
+    result.push_str("[");
+    for r in &results[0..results.len() - 1] {
+        if *r == zero || *r == one {
+            result.push_str(&r.to_string());
+        } else {
+            result.push_str(&format!("{:e}", r));
+        }
+        result.push_str(", ");
+    }
+    let r = &results[results.len()-1];
+    if *r == zero || *r == one {
+        result.push_str(&r.to_string());
+    } else {
+        result.push_str(&format!("{:e}", r));
+    }
+    result.push_str("]");
+
+    return result;
 }
 
 #[derive(StructOpt)]
@@ -69,6 +140,9 @@ enum Command {
 
         #[structopt(short = "s", long = "shots", default_value = "1024", help = "The number of shots")]
         shots:u32,
+
+        #[structopt(short = "o", long = "output", help = "Output filename")]
+        output:Option<PathBuf>
     },
     
     #[structopt(about = "Get the final real probabilities")]
@@ -77,7 +151,10 @@ enum Command {
         circuit_filename:PathBuf,
     
         #[structopt(short = "q", long = "qubits", help = "The number of qubits. Must be at least the width of the circuit.")]
-        qubit_count:Option<u8>
+        qubit_count:Option<u8>,
+
+        #[structopt(short = "o", long = "output", help = "Output filename")]
+        output:Option<PathBuf>
     },
     
     #[structopt(about = "Get the final statevector")]
@@ -86,6 +163,9 @@ enum Command {
         circuit_filename:PathBuf,
     
         #[structopt(short = "q", long = "qubits", help = "The number of qubits. Must be at least the width of the circuit.")]
-        qubit_count:Option<u8>
+        qubit_count:Option<u8>,
+
+        #[structopt(short = "o", long = "output", help = "Output filename")]
+        output:Option<PathBuf>
     }
 }
